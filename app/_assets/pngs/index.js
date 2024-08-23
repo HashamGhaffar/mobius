40ee16c4a4b980d6c6560d4f5021752a329b6a01
HashamGhaffar <hasham.ghaffar11223@gmail.com>
2024-08-24 00:54:47 +0500

    adding Marquee images

--- app/_assets/pngs/index.js
+++ app/_assets/pngs/index.js
@@ -7,7 +7,6 @@ import Lenz from "./lenz.png";
 import DarkSquareGridBackground from "./darkSquareGridBackground.png";
 import BulletsServicesList from "./bulletsServicesList.png";
 // slider images 10_num
-// import Avancus from "./avancus.png";
 import Avancus from "./projects/avancus.png";
 import Elivate from "./projects/elivate.png";
 import FoneStar from "./projects/foneStar.png";
@@ -23,6 +22,18 @@ import Discover from "./Discover.png";
 import ToadWithBeer from "./toadWithBeer.png";
 import BgJoinUs from "./BgJoinUs.png";
 
+// Marquee images 
+import MAvancus from "./marquee/Avancus.png";
+import MElivate from "./marquee/Elevate.png";
+import MFoneStar from "./marquee/Fonestar.png";
+import MFormulaZ from "./marquee/FormulaZ.png";
+import MHouduo from "./marquee/Houduo.png";
+import MLightOne from "./marquee/lightOne.png";
+import MLuna from "./marquee/Luna.png";
+import MMetaleon from "./marquee/Metaleon.png";
+import MPillPuncher from "./marquee/PillPuncher.png";
+import MSony from "./marquee/Sony.png";
+
 const pngs = {
   Heading,
   HeroHome,
@@ -32,6 +43,7 @@ const pngs = {
   Lenz,
   DarkSquareGridBackground,
   BulletsServicesList,
+
   // slider images_10num
   Avancus,
   Elivate,
@@ -47,6 +59,18 @@ const pngs = {
   Discover,
   ToadWithBeer,
   BgJoinUs,
+
+  // Marquee images 
+  MAvancus,
+  MElivate,
+  MFoneStar,
+  MFormulaZ,
+  MHouduo,
+  MLightOne,
+  MLuna,
+  MMetaleon,
+  MPillPuncher,
+  MSony,
 };
 
 export default pngs;
