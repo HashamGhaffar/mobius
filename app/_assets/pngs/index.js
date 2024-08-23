9908def5e34fa1fc2148338800c22b837e4d199e
HashamGhaffar <hasham.ghaffar11223@gmail.com>
2024-08-24 00:32:49 +0500

    mengeing directory of assets

--- app/_assets/pngs/index.js
+++ app/_assets/pngs/index.js
@@ -8,16 +8,16 @@ import DarkSquareGridBackground from "./darkSquareGridBackground.png";
 import BulletsServicesList from "./bulletsServicesList.png";
 // slider images 10_num
 // import Avancus from "./avancus.png";
-import Avancus from "../projects/avancus.png";
-import Elivate from "../projects/elivate.png";
-import FoneStar from "../projects/foneStar.png";
-import FormulaZ from "../projects/formulaZ.png";
-import Houduo from "../projects/houduo.png";
-import LightOne from "../projects/lightOne.png";
-import Luna from "../projects/luna.png";
-import Metaleon from "../projects/metaleon.png";
-import PillPuncher from "../projects/pillPuncher.png";
-import Sony from "../projects/sony.png";
+import Avancus from "./projects/avancus.png";
+import Elivate from "./projects/elivate.png";
+import FoneStar from "./projects/foneStar.png";
+import FormulaZ from "./projects/formulaZ.png";
+import Houduo from "./projects/houduo.png";
+import LightOne from "./projects/lightOne.png";
+import Luna from "./projects/luna.png";
+import Metaleon from "./projects/metaleon.png";
+import PillPuncher from "./projects/pillPuncher.png";
+import Sony from "./projects/sony.png";
 
 import Discover from "./Discover.png";
 import ToadWithBeer from "./toadWithBeer.png";
